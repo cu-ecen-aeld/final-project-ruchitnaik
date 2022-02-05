4e896f4a2dcf8e3c3da936e2c99ac97e865bcf08
ruchitnaik <ruchit.naik@colorado.edu>
2022-02-05 14:49:17 -0700

    syntax error rectified

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = eaa47fde1d867a41c834a89782ec67cbf89d1f8e
+AESD_ASSIGNMENTS_VERSION = 'eaa47fde1d867a41c834a89782ec67cbf89d1f8e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-ruchitnaik.git
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-ruchitnaik.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
